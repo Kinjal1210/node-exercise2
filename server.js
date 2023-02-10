98da73d5105f10e76aa419d298515b4cfa627bf9
Kunjalkumar Patel <120523967+Kinjal1210@users.noreply.github.com>
2023-02-10 15:03:19 -0500

    node-Exercise

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import apiRouter from "./routes";
 import config from "./config";
-// TODO: import router from routes/
+import {errorHandler} from "./middlewares/errorHandler.js";
+console.log(errorHandler);
+
 
 const app = express();
 
 app.use(express.json());
 
-// TODO: use the imported router to handle all requests
+app.use("/api", apiRouter);
+
+/**
+ * Error handler middleware
+ */
+app.use(errorHandler);
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.json({ name: err.name, msg: err.message });
+  res.json({ name: err.name, age: err.message });
 });
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}...`);
-});
+/**
+ * Bind the app to a specified port
+ * You can access your app at http://localhost:<port>
+ */
+app.listen(config.port || 5000, () =>
+  console.log(`Server listening on port ${config.port}...`)
+);
